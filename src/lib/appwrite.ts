48653b74abcd33dcc716808157ab4315d3e25af8
Pratham <pratham.sharma2105@gmail.com>
2024-10-04 22:57:14 +0530

    update appwrite and connected appwrite

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,14 @@
+import { Client, Account, Databases } from "appwrite";
+
+export const client = new Client();
+
+const { NEXT_PUBLIC_ENDPOINT, NEXT_PUBLIC_PROJECT_ID } = process.env;
+
+if (NEXT_PUBLIC_ENDPOINT === undefined || NEXT_PUBLIC_PROJECT_ID === undefined)
+  throw new Error("Appwrite ENV variables not exist.");
+
+client.setEndpoint(NEXT_PUBLIC_ENDPOINT).setProject(NEXT_PUBLIC_PROJECT_ID);
+
+export const account = new Account(client);
+export const db = new Databases(client);
+export { ID, Query } from "appwrite";
