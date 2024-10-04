1f4e9ba553310117c17af798577fe9e5d7ab8b61
Pratham <pratham.sharma2105@gmail.com>
2024-10-04 23:57:33 +0530

    save

---
+++ config.ts
@@ -0,0 +1,16 @@
+/*
+ * Config file for appwrite database, function, storage, etc.
+ * I am using my preferred way feel free to use your way of setting up appwrite
+ */
+
+export const database = "production";
+
+export const collections = {
+  musics: "musics",
+  messages: "messages",
+  sharedLinks: "sharedLinks",
+};
+export const storage = {
+  musics: "musicFiles",
+  albumArts: "albumArts",
+};
